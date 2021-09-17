15d724c63f90dc9392454dfae126866038d71a36
Fedor <f.pushkov@alumni.nsu.ru>
2021-09-17 10:41:06 +0300

    update angular verison

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("./dist/finodays2021-front"));
+
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", { root: "dist/<name-on-package.json>/" });
+});
+
+app.listen(process.env.PORT || 8080);
